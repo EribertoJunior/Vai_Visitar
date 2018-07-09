fa7e64b519396a00cc9162de6e32a3e5de378669
Eriberto <eriberto@rcnet.com.br>
2018-07-09 15:23:13 -0300

    pequena correção no broadcast de inicialização do S.O.

--- app/src/main/java/br/com/avancado/android/projeto/vaivisitar/broadcast_receiver/MyReceiver.kt
+++ app/src/main/java/br/com/avancado/android/projeto/vaivisitar/broadcast_receiver/MyReceiver.kt
@@ -5,14 +5,17 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import br.com.avancado.android.projeto.vaivisitar.Utils.Util
+import br.com.avancado.android.projeto.vaivisitar.services.BaixarImagens_IntentService
 
 class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
         if (Intent.ACTION_BOOT_COMPLETED == intent.action){
-
             Log.i(Util.TAG, "MyReceiver (systema iniciado)")
+
+            context.startService(Intent(context, BaixarImagens_IntentService::class.java)
+                    .putExtra("url", "https://i2.wp.com/blog.venturas.com.br/wp-content/uploads/2016/09/Skogarfoss.jpg"))
 
         }
 
